a570c750cbcab367a0df360318cd2f69f75da29e
david83748 <david.vashchenko@gmail.com>
2022-01-21 18:15:12 +0200

    ошиба reading 'then' не знаю в чем проблема

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+    
+
+
+
+//     fetch('http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+  function Weather(city) {
+      this.city = city
+  }
